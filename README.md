9a794d089589a948a75e308e0eb1febed077ba34
amavelino <amavelino>
2018-11-30 19:45:39 +0800

    Added PostgreSQL settings

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 "# game_db_165" 
+
+Django 2.1.3
+PostgreSQL 9.5.14
+
+Setup:
+pip install django
+pip install psycopg2
+
+Database setup:
+CREATE USER game_db_admin WITH PASSWORD 'game165dbpass';
+CREATE DATABASE game_db_165 OWNER game_db_admin;
+
+...
+
+python manage.py migrate
+
+
+# TODO:
+- requirements.txt for ease of installation in the future
+- add more features ha ha
--- game_db_proj/settings.py
+++ game_db_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g(cb+p^5f0f^kzg4!5jq^5^u#40=lh%dm%08vvbqph4^q4t^8t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'game_db_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'game_db_165',
+        'USER': 'game_db_admin',
+        'PASSWORD': 'game165dbpass',
+        'HOST': 'localhost',
+        'PORT': '5432' 
     }
 }
 
