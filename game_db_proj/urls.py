38be82be2112dc3a8b2edd771b3a855a82c43208
amavelino <amavelino>
2018-12-03 07:02:25 +0800

    Added simple login and logout. Haven't blocked some of the urls when you're not logged in.

--- game_db_proj/urls.py
+++ game_db_proj/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('gamedb.urls')),
-    path('', auth_views.LoginView.as_view(template_name='homepage.html'), name='home')
+    path('', TemplateView.as_view(template_name='homepage.html'), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
