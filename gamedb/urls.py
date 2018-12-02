613c7865e5f7f548ff3b7c491d82dccdd63b78a8
amavelino <amavelino>
2018-12-02 23:37:36 +0800

    Added games list. Made add-comment go under a game. No link going to it yet tho

--- gamedb/urls.py
+++ gamedb/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('add-company/', views.add_company, name='add_company'),
-	path('add-game/', views.add_game, name='add_game'),
-	path('add-comment/', views.add_comment, name='add_comment'),
+	path('company/add', views.add_company, name='add_company'),
+	path('games/add', views.add_game, name='add_game'),
+	path('games/', views.show_games, name='show_games'),
+	path('games/<int:gid>/add-comment/', views.add_comment, name='add_comment'),
 ]
--- gamedb/views.py
+++ gamedb/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Game
 from .forms import CompanyForm, GameForm, CommentForm
 
 # Create your views here.
@@ -22,12 +23,22 @@ def add_game(request):
 		form = GameForm()
 	return render(request, 'add.html', {'form':form, 'item':"Game"})
 
-def add_comment(request):
+def add_comment(request, gid):
+	game = Game.objects.get(pk=gid)
+
+
 	if request.method == "POST":
 		form = CommentForm(request.POST)
 		if form.is_valid():
 			comment = form.save(commit=False)
+			comment.date_created = timezone.now()
+			comment.date_last_edited = timezone.now()
+			comment.gid = gid
 			comment.save()
 	else:
 		form = CommentForm()
-	return render(request, 'add.html', {'form':form, 'item':"Comment"})
+	return render(request, 'add.html', {'form':form, 'item':"Comment", 'game':game.title})
+
+def show_games(request):
+	items = Game.objects.all()
+	return render(request, 'show-games.html', {'game_list':items})
