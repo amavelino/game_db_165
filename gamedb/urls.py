f089038c19da78ad843690fb346dc77feaca2971
amavelino <amavelino>
2018-12-06 22:43:02 +0800

    Added edit forms for companies and games. Also added display for companies.

--- gamedb/urls.py
+++ gamedb/urls.py
@@ -4,8 +4,13 @@ from . import views
 
 urlpatterns = [
 	path('company/add', views.add_company, name='add_company'),
+	path('company/', views.show_company, name='show_company'),
+	#path('company/<int:compid>', views.show_company_info, name='show_company_info'),
+	path('company/<int:compid>/edit', views.edit_company, name='edit_company'),
+
 	path('games/add', views.add_game, name='add_game'),
 	path('games/', views.show_games, name='show_games'),
 	path('games/<int:gid>', views.show_game_info, name='show_game_info'),
+	path('games/<int:gid>/edit', views.edit_game, name='edit_game'),
 	path('games/<int:gid>/add-comment/', views.add_comment, name='add_comment'),
 ]
--- gamedb/views.py
+++ gamedb/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 import datetime
 from django.utils import timezone
-from .models import Game, Comment
+from .models import Game, Comment, Company
 from .forms import CompanyForm, GameForm, CommentForm
 
 @login_required
@@ -16,6 +16,26 @@ def add_company(request):
 		form = CompanyForm()
 	return render(request, 'add.html', {'form':form, 'item': "Company"})
 
+def show_company(request):
+	items = Company.objects.all()
+	return render(request, 'show-company.html', {'comp_list':items})
+
+@login_required
+def edit_company(request, compid):
+	company_obj = Company.objects.get(pk=compid)
+
+	if request.method == "POST":
+		form = CompanyForm(request.POST, instance=company_obj)
+		if form.is_valid():
+			company = form.save(commit=False)
+			company.save()
+	else:
+		form = CompanyForm(instance=company_obj)
+	return render(request, 'edit.html', {'form':form, 'item': "Company"})
+
+
+
+
 @login_required
 def add_game(request):
 	if request.method == "POST":
@@ -26,6 +46,29 @@ def add_game(request):
 	else:
 		form = GameForm()
 	return render(request, 'add.html', {'form':form, 'item':"Game"})
+
+def show_games(request):
+	items = Game.objects.all()
+	return render(request, 'show-games.html', {'game_list':items})
+
+def show_game_info(request, gid):
+	game = Game.objects.get(pk=gid)
+	comments = Comment.objects.filter(gid=gid)
+
+	return render(request, 'show-game-info.html', {'game':game, 'comments':comments})
+
+@login_required
+def edit_game(request, gid):
+	game_obj = Game.objects.get(pk=gid)
+
+	if request.method == "POST":
+		form = GameForm(request.POST, instance=game_obj)
+		if form.is_valid():
+			game = form.save(commit=False)
+			game.save()
+	else:
+		form = GameForm(instance=game_obj)
+	return render(request, 'edit.html', {'form': form, 'item':"Game"})
 
 @login_required
 def add_comment(request, gid):
@@ -42,13 +85,3 @@ def add_comment(request, gid):
 	else:
 		form = CommentForm()
 	return render(request, 'add.html', {'form':form, 'item':"Comment", 'game':game.title})
-
-def show_games(request):
-	items = Game.objects.all()
-	return render(request, 'show-games.html', {'game_list':items})
-
-def show_game_info(request, gid):
-	game = Game.objects.get(pk=gid)
-	comments = Comment.objects.filter(gid=gid)
-
-	return render(request, 'show-game-info.html', {'game':game, 'comments':comments})
